def1d6696c01fd45000c010a970f9128c404f847
Vaclav Synacek <easek@gmail.com>
2016-08-29 21:05:21 +0200

    refactor for each to clean map

--- bin/run.js
+++ bin/run.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 'use strict';
 
 var config = require('config');
@@ -14,16 +15,10 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 var targetToPromise = require('../app/targetToPromise');
 
 
-app.use(function *(){
-  const resultPromises = []
-  for(let target of targets) {
-
-        var promise = targetToPromise(target);
-        resultPromises.push(promise);
-
-  }
+app.use(function*() {
+  var resultPromises = targets.map(targetToPromise);
   const result = yield resultPromises;
-  this.body = result.join("\n")+"\n"
+  this.body = result.join("\n") + "\n"
 });
 
 app.listen(port);
