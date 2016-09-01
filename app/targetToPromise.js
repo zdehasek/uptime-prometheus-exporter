87be9999822d1c66f90ebaf8cda95567a38f7a38
Vaclav Synacek <easek@gmail.com>
2016-09-01 18:56:22 +0200

    lint end some minor errors

--- app/targetToPromise.js
+++ app/targetToPromise.js
@@ -3,54 +3,56 @@
 const request = require('request');
 
 // cache for cookies and patters if run as server
-var cookies = {};
-var patterns = {};
+const cookies = {};
+const patterns = {};
 
-module.exports = function targetToPromise(target) {
-	let promise = new Promise((resolve, reject) => {
+module.exports = function targetToPromise (target) {
+    const promise = new Promise((resolve) => {
 
-		let URL = target.url;
-		cookies[URL] = cookies[URL] || request.jar();
+        const URL = target.url;
+        cookies[URL] = cookies[URL] || request.jar();
 
-		request({
-				method: 'GET',
-				url: URL,
-				jar: cookies[URL]
-			},
+        request({
+            method: 'GET',
+            url: URL,
+            jar: cookies[URL]
+        },
 
-			function(error, response, body) {
-				if (error) {
-					console.error("hit");
-					console.error(error.message);
-					resolve("urlcheck{url=\"" + URL + "\"} 0");
-				} else {
+            (error, response, body) => {
+                if (error) {
+                    console.error('hit');
+                    console.error(error.message);
+                    resolve(`urlcheck{url="${URL}"} 0`);
+                } else {
 
-					console.log(target.url + " " + response.statusCode);
-					if (response.statusCode == 200) {
-
-
-						if (target.pattern) {
-
-							// cache compiled patterns
-							patterns[URL] = patterns[URL] || eval(target.pattern);
-
-							if (patterns[URL].exec(body)) {
-								resolve("urlcheck{url=\"" + URL + "\"} 1");
-							} else {
-								resolve("urlcheck{url=\"" + URL + "\"} 0.5");
-							}
-
-						} else {
-							resolve("urlcheck{url=\"" + URL + "\"} 1");
-						}
+                    console.log(`${target.url} ${response.statusCode}`);
+                    if (response.statusCode === 200) {
 
 
-					} else {
-						resolve("urlcheck{url=\"" + URL + "\"} 0");
-					}
-				}
-			});
-	});
+                        if (target.pattern) {
 
-	return promise;
-}
+                            // cache compiled patterns
+                            /* eslint no-eval: 0 */
+                            // eval is bad, but no other way to put regex in json and then use it
+                            patterns[URL] = patterns[URL] || eval(target.pattern);
+
+                            if (patterns[URL].exec(body)) {
+                                resolve(`urlcheck{url="${URL}"} 1`);
+                            } else {
+                                resolve(`urlcheck{url="${URL}"} 0.5`);
+                            }
+
+                        } else {
+                            resolve(`urlcheck{url="${URL}"} 1`);
+                        }
+
+
+                    } else {
+                        resolve(`urlcheck{url="${URL}"} 0`);
+                    }
+                }
+            });
+    });
+
+    return promise;
+};
--- bin/run.js
+++ bin/run.js
@@ -4,51 +4,53 @@
 
 const config = require('config');
 const koa = require('koa');
-var co = require('co');
-
-
-var targets = config.get('targets');
+const co = require('co');
 const sanitizeProtocols = require('../app/sanitizeProtocols');
-var sanitizedTargets = sanitizeProtocols(targets);
-
-
-//Cheap and insecure: TODO: FIX!!!
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
 const targetToPromise = require('../app/targetToPromise');
 
-var formatResults = function*() {
-  const resultPromises = sanitizedTargets.map(targetToPromise);
-  const result = yield resultPromises;
-  return result.join("\n") + "\n";
+const targets = config.get('targets');
+const sanitizedTargets = sanitizeProtocols(targets);
 
-var cachedResults = "";
+// Cheap and insecure: TODO: FIX!!!
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-if (config.has('port')) { // if run with port parameter, then run server and return result on each HTTP GET
+const formatResults = function* () {
+    const resultPromises = sanitizedTargets.map(targetToPromise);
+    const result = yield resultPromises;
+    return `${result.join('\n')}\n`;
+};
 
-  const port = config.get('port');
-  const app = koa();
+let cachedResults = '';
 
-  if (config.has('interval')) {
-    setInterval(function() {
-      co(formatResults()).then((results) => {
-        cachedResults = results;
-      });
-    }, config.get('interval'));
-  }
+if (config.has('port')) {
+// if run with port parameter, then run server and return result on each HTTP GET
 
-  app.use(function*() {
-    if (cachedResults === "") {
-      this.body = (cachedResults = yield formatResults());
-    } else {
-      this.body = cachedResults;
+    const port = config.get('port');
+    const app = koa();
+
+    if (config.has('interval')) {
+        setInterval(() => {
+            co(formatResults())
+                        .then((results) => {
+                            cachedResults = results;
+                        });
+        }, config.get('interval'));
     }
-  });
 
-  app.listen(port);
+    app.use(function* () {
+        if (cachedResults === '') {
+            this.body = yield formatResults();
+        } else {
+            this.body = cachedResults;
+        }
+    });
+
+    app.listen(port);
 
 } else { // if not run with port parameter, then print out and exit, do not run server
-  co(formatResults()).then((results) => {
-    console.log(results);
-  });
+    co(formatResults())
+                .then((results) => {
+                    console.log(results);
+                });
 }
+
